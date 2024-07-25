7276a09e0107172c7cd9f5062f3e3413a488ae8a
Tristan Renaud <107555726+tristan-rnd@users.noreply.github.com>
2024-07-25 14:56:34 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Tristan Renaud",
+  title: "MSc Biomedical Image Processing",
+  email: "tristan.re@hotmail.fr",
+  gitHub: "tristan-rnd",
+  instagram: "",
+  linkedIn: "tristan--renaud",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
