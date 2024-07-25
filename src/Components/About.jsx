1ed6cf72f729ade65d9e8af49b8950a81cb866f5
Tristan Renaud <107555726+tristan-rnd@users.noreply.github.com>
2024-07-25 15:37:50 +0000

    Reduced images res

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/wavy-background.jpeg";
+import image from "../images/wavy-background.jpg";
 
 const imageAltText = "blue wavy abstract background";
 
